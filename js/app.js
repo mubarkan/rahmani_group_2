669f7e4fad97563b5784a88b21228664385c8031
IljaGoushcha <iljagou@yahoo.com>
2015-02-23 23:15:05 -0500

    added styling to footer

---
+++ js/app.js
@@ -0,0 +1,13 @@
+function adjustSlidesHeight() {
+  var height = $(window).height();
+  $('.slides-body').css({
+    'height': height-100
+  });
+}
+
+jQuery(document).ready(function() {
+
+  adjustSlidesHeight();
+  $(window).on('resize', adjustSlidesHeight);
+
+});
