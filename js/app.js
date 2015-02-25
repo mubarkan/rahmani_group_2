7d537e87c2cfb41be9cd5c45ac6b21deec4c95b8
IljaGoushcha <iljagou@yahoo.com>
2015-02-25 14:32:01 -0500

    adjusted js for fact-1, fact-3, fact-5, and title to be responsive

--- js/app.js
+++ js/app.js
@@ -17,6 +17,34 @@ function adjustSlidesHeight() {
     'height': height-180
   });
 }
+function adjustSlide02Text() {
+  var width = $(window).width();
+  var height = $('.slides-body').height();
+  var lineHeight1 = (30*height/700).toString() + "px";
+  var lineHeight2 = (28*height/700).toString() + "px";
+  var lineHeight3 = (26*height/700).toString() + "px";
+  console.log("inside adjustSlide02Text");
+  console.log(height);
+  $('.rg-facts').css({
+    'font-size': (40*width/1680)
+  });
+  $('#fact-1 p').css({
+    'font-size': (25*width/1680),
+    'line-height': lineHeight1
+  });
+  $('#fact-1').css({
+    'padding': (15*width/1680)
+  });
+  $('#fact-3 p').css({
+    'font-size': (26*width/1680),
+    'line-height': lineHeight1
+  });
+  $('#fact-5 p').css({
+    'font-size': (25*width/1680),
+    'line-height': lineHeight3,
+    'margin-bottom': (5*width/1680)
+  });
+}
 function nextSlide() {
   var currentSlideId = $('.active').attr('id');
   var currentSlideNumber = parseInt(currentSlideId.replace("slide-0", ""));
@@ -64,13 +92,13 @@ function previousSlide() {
 }
 function emptySlideOne() {
   $('#slide-01 img').animate({
-    'top': '22%',
+    'top': '18%',
     'opacity': 'hide'
   }, 500);
 }
 function populateSlideOne() {
   $('#slide-01 img').css({
-    'top': '25%',
+    'top': '20%',
     'display': 'block'
   }, 500);
 }
@@ -79,6 +107,7 @@ jQuery(document).ready(function() {
 
   adjustSlidesHeight();
   $(window).on('resize', adjustSlidesHeight);
+  $(window).on('resize', adjustSlide02Text);
   $('.arrow-up').on('click', nextSlide);
   $('.arrow-down').on('click', previousSlide);
 });
