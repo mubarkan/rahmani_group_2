74b0852ca790b63b68dd95c941deffeb42aef2eb
IljaGoushcha <iljagou@yahoo.com>
2015-02-24 20:17:10 -0500

    implemented basic architecture for a new carousel

--- js/app.js
+++ js/app.js
@@ -7,11 +7,30 @@ function adjustSlidesHeight() {
   $('#slide-01').css({
     'height': height-180
   });
+  $('#slide-02').css({
+    'height': height-180
+  });
+}
+function nextSlide() {
+  var currentSlideId = $('.active').attr('id');
+  var currentSlideNumber = parseInt(currentSlideId.replace("slide-0", ""));
+  if (currentSlideNumber === 2) {
+    var nextSlideNumber = 1;
+  } else {
+    var nextSlideNumber = currentSlideNumber + 1;
+  }
+  var nextSlideId = "slide-0" + nextSlideNumber.toString();
+  console.log(currentSlideId);
+  console.log(nextSlideId);
+  $('#' + currentSlideId).fadeOut(400);
+  $('#' + currentSlideId).removeClass('active');
+  $('#' + nextSlideId).fadeIn(400);
+  $('#' + nextSlideId).addClass('active');
 }
 
 jQuery(document).ready(function() {
 
   adjustSlidesHeight();
   $(window).on('resize', adjustSlidesHeight);
-
+  $('.arrow-down').on('click', nextSlide);
 });
